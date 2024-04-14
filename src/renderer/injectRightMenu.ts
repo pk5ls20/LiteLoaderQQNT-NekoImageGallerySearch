4aeeb7f21fe0204e3c36dd92088972e17eddb813
pk5ls20 <pk5ls20@outlook.com>
2024-04-15 01:14:13 +0800

    feat: i2i support for mface (MarketFace)

--- src/renderer/injectRightMenu.ts
+++ src/renderer/injectRightMenu.ts
@@ -2,6 +2,40 @@ import iconHtml from '../assets/logo.svg?raw';
 
 const menuID = "nekoimg-i2i-menu"
 
+class imageContainer {
+    src: string;
+
+    constructor(src: string) {
+        this.src = src;
+    }
+
+    async toBlob(): Promise<Blob> {
+        if (this.src.startsWith('data:')) {
+            return this.convertBase64ToBlob();
+        } else if (this.src.startsWith('appimg://')) {
+            return await this.convertImageUrlToBlob();
+        } else {
+            throw new Error("Unsupported src type");
+        }
+    }
+
+    convertBase64ToBlob() {
+        const base64Content = this.src.split(';base64,').pop();
+        const binary = atob(base64Content);
+        const length = binary.length;
+        const bytes = new Uint8Array(new ArrayBuffer(length));
+        for (let i = 0; i < length; i++) {
+            bytes[i] = binary.charCodeAt(i);
+        }
+        return new Blob([bytes], {type: 'image/png'});
+    }
+
+    async convertImageUrlToBlob() {
+        const pathContent = this.src.split('appimg://').pop();
+        return await window.imageSearch.getLocalFileAsBlob(decodeURIComponent(pathContent));
+    }
+}
+
 const addQContextMenu = (qContextMenu: Element, icon: string, title: string, callback: Function) => {
     if (qContextMenu.querySelector(`#${menuID}`) != null) return;
     const tempEl = document.createElement("div");
@@ -22,44 +56,44 @@ const addQContextMenu = (qContextMenu: Element, icon: string, title: string, cal
             (textElement as HTMLElement).innerText = title;
         }
     }
-    item.addEventListener("click", () => {
-        callback();
+    item.addEventListener("click", async ()  => {
+        await callback();
         qContextMenu.remove();
     });
     qContextMenu.appendChild(item);
 }
 
 export const addQContextMenuMain = async () => {
-    let isRightClick = false;
-    let imagePath = "";
+    let isRightClick: boolean = false;
+    let imageObject: imageContainer = null;
     let imgEl = null;
-    const haveImgContent = () => imgEl.classList.contains("image-content") && imgEl?.src?.startsWith("appimg://")
+    const haveImgContent = () => imgEl.classList.contains("image-content") && imgEl?.getAttribute('src')
     document.addEventListener("mouseup", (event) => {
         if (event.button === 2) {
             isRightClick = true;
             imgEl = event.target;
             if (haveImgContent()) {
-                imagePath = imgEl?.src?.replace("appimg://", "");
+                imageObject = new imageContainer(imgEl?.src?.toString());
             } else {
                 imgEl = null;
-                imagePath = "";
+                imageObject = null;
             }
         } else {
             isRightClick = false;
-            imagePath = "";
+            imageObject = null;
         }
     });
     new MutationObserver(() => {
         const qContextMenu = document.querySelector(".q-context-menu");
-        if (qContextMenu && imagePath) {
-            let localPath = decodeURIComponent(imagePath);
+        if (qContextMenu && imageObject) {
+            const currentImageObject = imageObject;
             addQContextMenu(
                 qContextMenu,
                 iconHtml,
                 "Image Search",
                 async () => {
-                    let picBlob = await window.imageSearch.getLocalFileAsBlob(localPath);
-                    window.imageSearch.postAppImageSearchReq(picBlob);
+                    const fileBlobContent = await currentImageObject.toBlob();
+                    window.imageSearch.postAppImageSearchReq(fileBlobContent);
                 }
             );
         }
